3601235c4539e0aebe5937e7d349bbefd4f2a421
Dominic <dominic.pr93@gmail.com>
2019-11-22 20:12:58 +0100

    added switerjs

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,29 @@
 // scripts
+
+$(document).ready(function() {
+
+  //initialize swiper when document ready
+  let mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+
+});
