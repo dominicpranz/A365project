81bfcf1b5f34c66637a64e2c580ae2623e0c72a6
Dominic <dominic.pr93@gmail.com>
2019-11-23 00:03:18 +0100

    added a lot of project tech basics

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,24 @@
 
 $(document).ready(function() {
 
-  //initialize swiper when document ready
+  // initialize swiper when document ready
   let mySwiper = new Swiper('.swiper-container', {
+    speed: 900,
+    spaceBetween: 100,
+
     // Optional parameters
-    direction: 'vertical',
-    loop: true,
+    direction: 'horizontal',
+    //loop: true,
+
+    autoplay: {
+      delay: 3000,
+    },
+
+    effect: 'coverflow',
+    coverflowEffect: {
+      rotate: 30,
+      slideShadows: false,
+    },
 
     // If we need pagination
     pagination: {
@@ -19,11 +32,58 @@ $(document).ready(function() {
       prevEl: '.swiper-button-prev',
     },
 
-    // And if we need scrollbar
+    // And if we need scrollbar (shows incorrectly in case of loop true)
     scrollbar: {
       el: '.swiper-scrollbar',
     },
+
   });
+
+
+  // calculate year automatically
+  let currentYear = new Date().getFullYear();
+  $('#current-year').text(currentYear);
+
+  // handle form submission
+  $('#form').submit((e) => {
+    e.preventDefault();
+    let formdata = {};
+    formdata['name'] = $('#form input[name="user_name"]').val();
+    formdata['email'] = $('#form input[name="user_email"]').val();
+    formdata['message'] = $('#form textarea[name="user_message"]').val();
+
+    console.log(formdata);
+
+    var jqxhr = $.ajax({
+      type: 'POST',
+      url: 'php/form.php',
+      data: formdata, //JSON.stringify(formdata),
+    });
+
+
+    jqxhr.done((data) => {
+      console.log('done');
+      let answer = JSON.parse(data);
+      console.log(answer);
+      writeFormMessage(answer['message'], false);
+    });
+
+    jqxhr.fail((error) => {
+      console.log('error');
+      console.error(error);
+    });
+
+  });
+
+  function writeFormMessage(message, error) {
+    let formMessageEl = $('.form-message');
+    formMessageEl.text(message);
+    if (error) {
+      formMessageEl.addClass('error');
+    } else {
+      formMessageEl.removeClass('error');
+    }
+  }
 
 
 });
--- php/form.php
+++ php/form.php
@@ -2,4 +2,11 @@
 
 // a simple php file to answer to ajax calls from a filled in form
 
-$test = 1;
+$formdata = $_POST; // alternatevly you could get the raw data via file_get_contents("php://input") and then urldecode() or json_decode - POST handles this for us :)
+
+  $answer = [
+    'code' => 000,
+    'message' => 'Something went wrong',
+  ];
+
+echo json_encode($answer);
